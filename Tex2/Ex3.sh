a1de84c254160df7dabd8f4ff56fda264a4395a2
BBAvants <stnava@gmail.com>
2014-04-29 21:50:27 -0400

    WIP: ... mods but lots of work still needed

--- Tex2/Ex3.sh
+++ Tex2/Ex3.sh
@@ -1,2 +1,3 @@
-antsRegistrationSyNQuick.sh -d 2 -f r16slice.nii.gz -m r64slice.nii.gz -o output$0 -t r
+imgs=" -f r16slice.nii.gz -m r64slice.nii.gz "
+antsRegistrationSyNQuick.sh -d 2 $imgs -o output$0 -t r
 rm output$0* # cleanup
