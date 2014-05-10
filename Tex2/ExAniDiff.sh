e5db8f14513bfef03b8ff8cf71ebb8a314116108
BBAvants <stnava@gmail.com>
2014-05-10 19:18:51 -0400

    ENH: new atropos and anidiff listing

---
+++ Tex2/ExAniDiff.sh
@@ -0,0 +1,7 @@
+bn=output` echo $0 | cut -d '.' -f 1`
+ConvertToJpg r64slice.nii.gz Figures/${bn}ad1.jpg
+ImageMath 2 ${bn}ad.nii.gz PeronaMalik r64slice.nii.gz 20 0.5 
+ConvertToJpg ${bn}ad.nii.gz Figures/${bn}ad2.jpg
+ImageMath 2 ${bn}ad.nii.gz PeronaMalik r64slice.nii.gz 200 0.5 
+ConvertToJpg ${bn}ad.nii.gz Figures/${bn}ad3.jpg
+rm ${bn}* # cleanup
