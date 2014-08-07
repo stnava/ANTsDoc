8a63feb3b359c761340bc02098e4ecc440331667
BBAvants <stnava@gmail.com>
2014-08-07 17:52:27 -0400

    WIP: figure stuff

--- ANTsTalk/README.R
+++ ANTsTalk/README.R
@@ -10,4 +10,12 @@ srcdir<-"./"
 pre='antstalk'
 # render( paste(pre,".Rmd",sep=''), clean=TRUE, "revealjs_presentation" ) 
 render( paste(pre,".Rmd",sep=''), clean=TRUE, "ioslides_presentation" ) 
+# render( paste(pre,".Rmd",sep=''), clean=TRUE, "beamer_presentation" ) 
 
+ss<-"Im interested in your brain alignment algorithms, and why you think
+they are the best.  I'm also interested in whether your software
+implementations will scale up to whole brain microscopy datasets, which
+will be much larger than MRI datasets.
+
+ highlighting and explaining the fundamental
+computational problems will be important."
